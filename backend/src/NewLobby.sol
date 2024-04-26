3c6409225039db528153e43e99b4dda36144cb77
Emirhan-Cavusoglu-sftw <emirhancavusoglu.sftw@gmail.com>
2024-04-26 18:48:44 +0300

    vote function edited

--- backend/src/NewLobby.sol
+++ backend/src/NewLobby.sol
@@ -14,13 +14,16 @@ contract Lobby {
         Role role;
         uint256 votes;
     }
-    
+
+    event timeUpdate();
+    address[] public targets;
     uint public timeCounter;
     Player[] public players;
+    mapping(address => Player) public playerMap;
 
     bool public gunduz = false;
 
-    function createAccount(string memory _nickname, address _userID) external {}
+    
 
     function joinLobby(
         string memory _nickName
@@ -32,16 +35,43 @@ contract Lobby {
             role: Role.None,
             votes: 0
         });
-        players.push(newPlayer);
+        // players.push(newPlayer);
+        playerMap[msg.sender] = newPlayer;
      
      timeCounter = block.timestamp;
     }
     
     
+    function vote(address target) public {
+        require(block.timestamp <= timeCounter + 40 seconds, "Time has not passed yet");
+        require(gunduz == true, "It is not night time");
+        require(playerMap[msg.sender].role != Role.None, "Only villagers can vote");
+        require(playerMap[target].role != Role.None, "You can't vote for a player who is not in the game");
+        playerMap[target].votes++;
+        targets.push(target);
+        for(uint i = 0; i < targets.length; i++){
+            if(targets[i] == target){
+                return;
+            }
+        }
+        
+        
+
+        timeCounter = block.timestamp;
+        gunduz = !gunduz;
+        emit timeUpdate();
+       
+
+    }
 
    
     function kill() public {
-        require(timeCounter <= block.timestamp + 30 seconds, "Time has not passed yet");
+        require(block.timestamp <= timeCounter + 40 seconds, "Time has not passed yet");
+        require(gunduz == false, "It is not day time");
+        timeCounter = block.timestamp;
+        gunduz = !gunduz;
+        emit timeUpdate();
+        
     }
 
 
