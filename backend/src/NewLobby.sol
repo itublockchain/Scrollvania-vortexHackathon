f05647a3ec46fc3e2fee7477b2406f853dff85ef
0xbeyza <mbeyzaoral9@gmail.com>
2024-04-27 21:00:29 +0300

    gas details fixed

--- backend/src/NewLobby.sol
+++ backend/src/NewLobby.sol
@@ -1,11 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+
+/*Yapılacaklar
+
+    oyları sıfırla
+    oyunu başlatan fonksiyon
+    vampiri ata
+    oylamayı başlat
+
+
+
+ */
+
+
 contract Lobby {
     enum Role {
-        Vampyre,
+        Vampire,
         Villager,
-        None
+        Dead
     }
 
     struct Player {
@@ -25,6 +38,7 @@ contract Lobby {
     uint public timeCounter;
     Player[] public players;
     mapping(address => Player) public playerMap;
+    uint256 public playerCount;
 
     modifier countTime {
         require(block.timestamp <= timeCounter + 40 seconds, "Time has not passed yet");
@@ -32,29 +46,48 @@ contract Lobby {
     }
 
     bool public gunduz = false;
+    HighestVote private highestVote;
 
     event vampireFound(address vampireAddress);
+    event gameStarted;
+    event murdered(address victim);
 
     function joinLobby(
         string memory _nickName
     ) external payable {
         require(msg.value == 0.0001 ether, "Transaction failed");
+        require(playerCount<10,"The lobby is full!";)
+
+        // rol atasana elemana
+
+
         Player memory newPlayer = Player({
             nickname: _nickName,
             userID: msg.sender,
-            role: Role.None,
+            role: Role.Villager,
             votes: 0
         });
         // players.push(newPlayer);
         playerMap[msg.sender] = newPlayer;
+        playerCount++;
      
-     timeCounter = block.timestamp;
+     /*timeCounter = block.timestamp;
      if(players.length == 10){
         //fonksiyon döndür
-     }
+     }*/ 
+        if(playerCount==10){
+            // oyunu başlat
+            uint randNum = block.timestamp;
+            players[randNum%2+1].role =Role.Vampire;
+            players[randNum].role = Role.Vampire;
+
+
+
+            emit gameStarted;
+        }
     }
     
-    HighestVote private highestVote;
+    
     function vote(address target) public countTime { //general voting function
         //require(block.timestamp <= timeCounter + 40 seconds, "Time has not passed yet"); modifier added
         require(gunduz == true, "It is not day time");
@@ -66,26 +99,6 @@ contract Lobby {
             highestVote.voteNumber = playerMap[target].votes;
             highestVote.playerAddress = target;
         }
-
-
-       // uint256 highestVote;
-       /* for(uint i = 0; i < targets.length; i++){
-            if(playerMap[targets[i]].votes > playerMap[targets[i+1]].votes){
-                highestVote = playerMap[targets[i]].votes;
-            }
-        }*/
-
-
-
-
-
-      /*  for(uint i = 0; i < targets.length; i++){
-            if(playerMap[targets[i]].votes == highestVote){
-                playerMap[targets[i]].role = Role.None;
-            }
-        }*/
-        
-
         timeCounter = block.timestamp;
         gunduz = !gunduz;
         emit timeUpdate();
@@ -98,16 +111,18 @@ contract Lobby {
             emit vampireFound(highestVote.playerAddress);
             return;
         }
-        playerMap[highestVote.playerAddress].role=Role.None;
+        playerMap[highestVote.playerAddress].role=Role.Dead;
+        highestVote.voteNumber=0;
+        highestVote.playerAddress="";
     }
     
     
-    function kill(address target) public countTime{
+    function kill(address target) private countTime{
        // require(block.timestamp <= timeCounter + 40 seconds, "Time has not passed yet");
         require(gunduz == false, "It is not night time");
-        require(playerMap[msg.sender].role == Role.Vampyre, "Only vampyres can vote");
-        require(playerMap[target].role != Role.None, "You can't vote for a player who is not in the game");
-        playerMap[target].votes++;
+        require(playerMap[msg.sender].role == Role.Vampire, "Only vampires can kill");
+        require(playerMap[target].role != Role.None, "You can't kill a player who is not in the game");
+      /*  playerMap[target].votes++;
         targets.push(target);
         for(uint i = 0; i < targets.length; i++){
             if(playerMap[targets[i]].votes > playerMap[targets[i+1]].votes){
@@ -120,7 +135,11 @@ contract Lobby {
                 playerMap[targets[i]].role = Role.None;
             }
         }
-        
+        */
+
+
+        playerMap[target].role=Role.Dead;
+        emit murdered(target);
         timeCounter = block.timestamp;
         gunduz == gunduz;
         emit timeUpdate();
