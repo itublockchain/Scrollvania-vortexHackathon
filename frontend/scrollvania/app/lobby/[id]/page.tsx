e2e014f21d8f9eeffc62644454a3160602aedf1b
LandoC4lrissian <ahmetatylz55@gmail.com>
2024-04-28 21:56:19 +0300

    game page design

--- frontend/scrollvania/app/lobby/[id]/page.tsx
+++ frontend/scrollvania/app/lobby/[id]/page.tsx
@@ -3,9 +3,23 @@ import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useWatchContractEvent } from "wagmi";
 import { scrollSepolia } from "wagmi/chains";
+import Image from "next/image";
 
 const LobiPage = () => {
   const [lobbyCode, setLobbyCode] = useState("");
+
+  const images = [
+    "/kedi.png",
+    "/ghost.png",
+    "/frank.png",
+    "/vampirrr.png",
+    "/witch.png",
+    "/kedi.png",
+    "/ghost.png",
+    "/frank.png",
+    "/vampirrr.png",
+    "/witch.png",
+  ];
 
   async function StartGame() {
     useWatchContractEvent({
@@ -28,8 +42,82 @@ const LobiPage = () => {
 
   return (
     <>
-      <div className="min-h-screen bg-[url('/bgImage.png')] bg-center bg-cover ">
-        <div className="w-16 h-16 rounded-full bg-white">
+      <div className="min-h-screen bg-[url('/bgImage.png')] bg-center bg-cover flex items-center justify-center">
+        <div className="relative w-full flex flex-col items-center">
+          <div className="circle">
+            {Array(10)
+              .fill(null)
+              .map((_, index) => (
+                <div
+                  key={index}
+                  className="w-24 h-24 rounded-full bg-white item flex justify-center items-center ml-[550px]"
+                >
+                  <Image
+                    src={images[index]}
+                    alt={`Picture ${index + 1}`}
+                    width={60}
+                    height={60}
+                  />
+                </div>
+              ))}
+            {/* <div className="">
+              <Image
+                src={"/vampire.png"}
+                alt="vampire"
+                width={300}
+                height={300}
+              />
+            </div> */}
+          </div>
+        </div>
+
+        <style jsx>{`
+          .circle {
+            display: flex;
+            justify-content: flex-start;
+            align-items: center;
+            position: relative;
+            width: 100%;
+            height: 300px;
+          }
+          .item {
+            position: absolute;
+            transform: rotate(calc(36deg * var(--i))) translate(350px)
+              rotate(calc(-36deg * var(--i)));
+          }
+          .item:nth-child(1) {
+            --i: 0;
+          }
+          .item:nth-child(2) {
+            --i: 1;
+          }
+          .item:nth-child(3) {
+            --i: 2;
+          }
+          .item:nth-child(4) {
+            --i: 3;
+          }
+          .item:nth-child(5) {
+            --i: 4;
+          }
+          .item:nth-child(6) {
+            --i: 5;
+          }
+          .item:nth-child(7) {
+            --i: 6;
+          }
+          .item:nth-child(8) {
+            --i: 7;
+          }
+          .item:nth-child(9) {
+            --i: 8;
+          }
+          .item:nth-child(10) {
+            --i: 9;
+          }
+        `}</style>
+
+        <div className="bg-white opacity-80 w-96 h-96 rounded-3xl absolute right-44">
           
         </div>
       </div>
